31d7309005354e15d02ad708863f3ffee2679e38
Mayank Naruka <mayanknaruka47@gmail.com>
2024-12-06 15:39:38 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai (){
+    // named IIFE
+    console.log(`DB connected`)
+}) ();
+
+( (name) => {
+    console.log(`DB connected two ${name}`);
+    
+})("mayank")
