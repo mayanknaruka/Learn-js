10d1bb1696290566211d0c332d51d0f6cc33735f
Mayank Naruka <mayanknaruka47@gmail.com>
2024-12-11 10:41:06 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// while loop 
+// let index = 11
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//    index = index + 2
+// }   
+
+let myArray = ["batsman", "captain america", "thor"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`value is ${score}`);
+    score++
+} while (score <=10 ); 
